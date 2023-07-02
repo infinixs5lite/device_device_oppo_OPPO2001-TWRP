f346c4ba2055b6f5b9c78be79505e91b84332c80
infinixs5lite <kelvinchinedu97@gmail.com>
2023-07-02 10:20:42 +0100

    Update twrp_OP4B9B.mk

--- twrp_OP4B9B.mk
+++ twrp_OP4B9B.mk
@@ -2,6 +2,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
+# Inherit from OP4B9B device
+$(call inherit-product, device/oppo/OP4B9B/device.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
+
 PRODUCT_DEVICE := OP4B9B
 PRODUCT_NAME := twrp_$(PRODUCT_DEVICE)
 PRODUCT_BRAND := OPPO
