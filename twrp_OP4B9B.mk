d6c6dbd9d1344a51f48222fd5af0972db3eae855
infinixs5lite <kelvinchinedu97@gmail.com>
2023-07-02 10:34:53 +0100

    Update twrp_OP4B9B.mk

--- twrp_OP4B9B.mk
+++ twrp_OP4B9B.mk
@@ -3,11 +3,11 @@ $(call inherit-product, vendor/pb/config/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 PRODUCT_DEVICE := OP4B9B
-PRODUCT_NAME := twrp_$(PRODUCT_DEVICE)
+PRODUCT_NAME := twrp_OP4B9B
 PRODUCT_BRAND := OPPO
 PRODUCT_MODEL := CPH2021
 PRODUCT_MANUFACTURER := OPPO
 
 PRODUCT_SHIPPING_API_LEVEL := 28
 
-DEVICE_PATH := device/$(PRODUCT_BRAND)/$(PRODUCT_DEVICE)
+DEVICE_PATH := device/OPPO/OP4B9B
