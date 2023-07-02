aad7fe847515ab20388fb6c5a67171f20195daf6
infinixs5lite <kelvinchinedu97@gmail.com>
2023-07-02 10:28:07 +0100

    Update twrp_OP4B9B.mk

--- twrp_OP4B9B.mk
+++ twrp_OP4B9B.mk
@@ -1,5 +1,5 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 PRODUCT_DEVICE := OP4B9B
